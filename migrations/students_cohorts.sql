7ad11c0f0177f62281777e48776f4ed306147cf0
donnyli95 <sz3li@uwaterloo.ca>
2021-04-26 19:50:52 +0000

    has two new tables and more fake data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -5,7 +5,7 @@ CREATE TABLE cohorts (
   end_date DATE
 );
 
-CREATE TABLE student (
+CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
