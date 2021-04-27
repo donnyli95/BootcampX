f2368f442e8eab2894882f520ff503dfce586dfb
donnyli95 <sz3li@uwaterloo.ca>
2021-04-27 22:44:55 +0000

    uses parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,17 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
-const limit = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohort}%`, limit]
 
-pool.query(`
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
